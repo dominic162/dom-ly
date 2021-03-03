2e0dc48f593bcc98213858b2e4b257d17819ef17
Nischey <heynischey@gmail.com>
2021-03-03 23:32:05 +0530

    added https

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,35 +16,33 @@ def dashboard(request):
     }
     if request.method=="POST":
         context['form']=forms.urlmanipulate(request.POST)
-        if request.POST['originalurl']!='' and request.POST['shorturl']!='':
+        if request.POST['originalurl']!='':
             usr=request.user
             originalurl=request.POST['originalurl']
-            context['originalurl']=originalurl
-            shorturl=request.POST['shorturl']
-            context['shorturl']=shorturl
-            try:
-                element=models.urlshort.objects.get(short_url=shorturl)    
-                context['error']='That URL is already taken. Try a different custom url or leave blank to auto generate'
-            except:
-                obj=models.urlshort(original_url=originalurl,short_url=shorturl,user=usr)
-                obj.save()
-                context['success']=True
-                context['shorturl']=urllib.parse.quote(shorturl)
-        elif request.POST['originalurl']!='':
-            originalurl=request.POST['originalurl']
-            context['originalurl']=originalurl
-            usr=request.user
-            generated=False
-            while not generated:
-                shorturl=short()
+            if 'https://' not in originalurl:
+                originalurl='https://'+originalurl
+            if request.POST['shorturl']!='':
+                shorturl=request.POST['shorturl']
                 try:
-                    element=models.urlshort.objects.get(short_url=shorturl)
+                    element=models.urlshort.objects.get(short_url=shorturl)    
+                    context['error']='That URL is already taken. Try a different custom url or leave blank to auto generate'
                 except:
                     obj=models.urlshort(original_url=originalurl,short_url=shorturl,user=usr)
                     obj.save()
-                    generated=True
-            context['shorturl']=urllib.parse.quote(shorturl)
-            context['success']=True
+                    context['success']=True
+                    context['shorturl']=urllib.parse.quote(shorturl)
+            else:
+                generated=False
+                while not generated:
+                    shorturl=short()
+                    try:
+                        element=models.urlshort.objects.get(short_url=shorturl)
+                    except:
+                        obj=models.urlshort(original_url=originalurl,short_url=shorturl,user=usr)
+                        obj.save()
+                        generated=True
+                context['shorturl']=urllib.parse.quote(shorturl)
+                context['success']=True
         else:
             context['error']='Enter a valid url to shorten'
     return render(request,'home.html',context)
